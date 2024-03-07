7d40ae183cfe63951f62d125c9058fb74e8831c7
Kauan Rodrigues Oliveira Rocha <kauan.rocha@yuzer.com.br>
2024-03-07 19:31:39 -0300

    Implements rollback to createTransaction

--- src/main/java/com/rinha/backend/repositories/TransacaoRepository.java
+++ src/main/java/com/rinha/backend/repositories/TransacaoRepository.java
@@ -1,8 +1,10 @@
 package com.rinha.backend.repositories;
 
 import com.rinha.backend.models.Transacao;
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
@@ -18,21 +20,16 @@ public class TransacaoRepository {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    @Transactional
-    public Map<String, Integer> salvarTransacao(Transacao transacao, int limiteCliente) {
-        int saldoCliente = jdbcTemplate.queryForObject("SELECT saldo FROM clientes WHERE id = ?", Integer.class, transacao.getClienteId());
-
-        if (saldoCliente - transacao.getValor() < -limiteCliente) {
-            throw new RuntimeException("Transação não permitida: ultrapassaria o limite do cliente");
-        }
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public int salvarTransacao(Transacao transacao, int limiteCliente) {
         jdbcTemplate.update("INSERT INTO transacoes (valor, cliente_id, tipo, descricao) VALUES (?, ?, ?, ?); " +
                         "UPDATE clientes SET saldo = saldo - ?, limite = ? WHERE id = ?", transacao.getValor(), transacao.getClienteId(), transacao.getTipo(), transacao.getDescricao(),
                 transacao.getValor(), limiteCliente, transacao.getClienteId());
 
-        Map<String, Integer> saldoLimite = new HashMap<>();
-        saldoLimite.put("limite", limiteCliente);
-        saldoLimite.put("saldo", saldoCliente - transacao.getValor());
-
-        return saldoLimite;
+        int saldoCliente = jdbcTemplate.queryForObject("SELECT saldo FROM clientes WHERE id = ? FOR UPDATE", Integer.class, transacao.getClienteId());
+        if (saldoCliente < limiteCliente) {
+            throw new RuntimeException("Transação não permitida: ultrapassaria o limite do cliente");
+        }
+        return saldoCliente;
     }
 }
